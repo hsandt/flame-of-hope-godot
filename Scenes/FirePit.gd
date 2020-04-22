a3e1178310f2389e2a04b12f90aaa9fc866549c2
Hyper Sonic <hs@fuwar.in>
2020-04-22 21:13:58 +0100

    [EDITOR] Preview FirePit state with tool script

--- Scenes/FirePit.gd
+++ Scenes/FirePit.gd
@@ -1,6 +1,7 @@
+tool
 extends Node2D
 
-export var lit_on_start := false
+export var lit_on_start := false setget set_lit_on_start
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -10,3 +11,10 @@ func _ready():
 	else:
 		animation_player.play("FirePit_Unlit")
 		
+func set_lit_on_start(new_lit_on_start):
+	lit_on_start = new_lit_on_start
+	
+	if Engine.editor_hint:
+		# in game runtime we play FirePit_Lit/Unlit which also changes the Body
+		# sprite, but for preview in editor, showing/hiding Flame is enough
+		$Flame.visible = new_lit_on_start
