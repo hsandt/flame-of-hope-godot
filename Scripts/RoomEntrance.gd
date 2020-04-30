1d5a52864ba397869e849fe375334f652096848c
huulong <n.huu.long@gmail.com>
2020-04-30 16:17:14 +0100

    [LEVEL] Added Room2 with RoomEntrance for camera smooth update

---
+++ Scripts/RoomEntrance.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+onready var camera := $"/root/Dungeon/Camera2D" as Camera2D
+
+func _on_RoomEntrance_body_entered(body):
+	# Collision is set with Character only, so we don't need to check for body nature,
+	# we know it's the character
+	# Move the camera to the Room (parent) position, since it is anchored Fixed TopLeft
+	camera.position = get_parent().position
