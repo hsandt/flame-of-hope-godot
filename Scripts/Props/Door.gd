0ef130e9e9d250f702225efbedf7d73898530005
Hyper Sonic <hs@fuwar.in>
2020-05-05 23:38:34 +0100

    [DOOR] Disconnect opening ignitables on open to avoid reopen Jingle

--- Scripts/Props/Door.gd
+++ Scripts/Props/Door.gd
@@ -16,19 +16,24 @@ onready var jingle_player := $"/root/Dungeon/JinglePlayer" as AudioStreamPlayer
 onready var collision_shape := $StaticBody2D/CollisionShape2D as CollisionShape2D
 
 func _ready():
+	var invalid_ignitable_paths := []
+	
 	for ignitable_path in opening_ignitable_paths:
 		if ignitable_path.is_empty():
-			print("WARNING: Door '%s' references opening ignitable with empty path. Delete it from the array for cleanup" % get_path())
+			invalid_ignitable_paths.append(ignitable_path)
+			print("WARNING: Door '%s' references opening ignitable with empty path. It will be removed at runtime, but you should delete it in the Inspector" % get_path())
 			continue
 		
 		var ignitable = get_node(ignitable_path)
 		if not ignitable:
-			print("WARNING: Door '%s' references opening ignitable with path: '%s', but it is null" % [get_path(), ignitable_path])
+			invalid_ignitable_paths.append(ignitable_path)
+			print("WARNING: Door '%s' references opening ignitable with path: '%s', but it is null. It will be removed at runtime." % [get_path(), ignitable_path])
 			continue
 		
 		ignitable = ignitable as Ignitable
 		if not ignitable:
-			print("WARNING: Door '%s' references opening ignitable %s, but it is not an Ignitable" % [get_path(), ignitable_path])
+			invalid_ignitable_paths.append(ignitable_path)
+			print("WARNING: Door '%s' references opening ignitable %s, but it is not an Ignitable. It will be removed at runtime." % [get_path(), ignitable_path])
 			continue
 
 			# deferred connection to avoid disabling collision during physics process (only matters for lit signal)
@@ -41,6 +46,10 @@ func _ready():
 		# only count valid ignitables to avoid getting stuck in case
 		# we prepared an array too big (e.g. size = 4 but filled only 3)
 		_opening_ignitable_count += 1
+		
+	# cleanup any invalid node path (just so _open doesn't have to check them again before disconnect)
+	for invalid_ignitable_path in invalid_ignitable_paths:
+		opening_ignitable_paths.erase(invalid_ignitable_path)
 
 func _on_opening_ignitable_lit():
 	# increment count, and open door if all connected ignitables have been lit
@@ -57,6 +66,11 @@ func _on_opening_ignitable_unlit():
 func _open():
 	visible = false
 	collision_shape.disabled = true
+	
+	for ignitable_path in opening_ignitable_paths:
+		var ignitable = get_node(ignitable_path)
+		ignitable.disconnect("lit", self, "_on_opening_ignitable_lit")
+		ignitable.disconnect("unlit", self, "_on_opening_ignitable_unlit")
 	
 	# audio
 	jingle_player.stream = victory_jingle
