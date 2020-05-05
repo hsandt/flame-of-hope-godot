ce11c064ee0d038474c8781e5248d3fec9ebde13
Hyper Sonic <hs@fuwar.in>
2020-05-05 17:14:30 +0100

    [CAMERA] Fixed camera offset on start and on room entrances

--- Scripts/RoomEntrance.gd
+++ Scripts/RoomEntrance.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
+# Vertical offset between room topleft and camera so camera is centered on room
+export(float) var camera_offset_y = 1
+
 onready var camera := $"/root/Dungeon/Camera2D" as Camera2D
 
 func _on_RoomEntrance_body_entered(_body):
 	# Collision is set with Character only, so we don't need to check for body nature,
 	# we know it's the character
 	# Move the camera to the Room (parent) position, since it is anchored Fixed TopLeft
-	camera.position = get_parent().position
+	camera.position = get_parent().position + camera_offset_y * Vector2.DOWN
