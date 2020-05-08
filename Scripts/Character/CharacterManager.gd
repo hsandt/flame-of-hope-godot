4e6df9a594d577e12e9805d083d1ccf776641c00
Hyper Sonic <hs@fuwar.in>
2020-05-08 20:31:35 +0100

    [LEVEL] Room3 draft, debug to start in any room and see Room number

---
+++ Scripts/Character/CharacterManager.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+onready var character := $"../Character" as Node2D
+
+# Initial room (0 in normal game, change for debug)
+export(int) var start_room_index = 0
+
+func _ready():
+	# room 1 entrance is at 240, 268 and each room has a height of 16*17=272
+	character.global_position = Vector2(240, 268 - 272 * start_room_index)
--- Scripts/RoomEntrance.gd
+++ Scripts/RoomEntrance.gd
@@ -4,9 +4,13 @@ extends Area2D
 export(float) var camera_offset_y = 1
 
 onready var camera := $"/root/Dungeon/Camera2D" as Camera2D
+onready var level_label := $"/root/Dungeon/CanvasLayer/Level" as Label
 
 func _on_RoomEntrance_body_entered(_body):
 	# Collision is set with Character only, so we don't need to check for body nature,
 	# we know it's the character
 	# Move the camera to the Room (parent) position, since it is anchored Fixed TopLeft
 	camera.position = get_parent().position + camera_offset_y * Vector2.DOWN
+	
+	# show room name for debug (name taken from Room root)
+	level_label.text = get_parent().name
