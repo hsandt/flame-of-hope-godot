5930644063f9fc4e3681238a8ff8054b9873c67e
Hyper Sonic <hs@gamedesignshortcut.com>
2020-07-18 23:45:50 +0200

    [TRIGGER] IgnitionTrigger doesn't check all reasons failing ignitable get_node Instead, just check with assert_node_got_by_path

--- Scripts/Event/IgnitionTrigger.gd
+++ Scripts/Event/IgnitionTrigger.gd
@@ -28,53 +28,26 @@ var _trigger_ignitable_lit_count := 0
 
 
 func _ready():
-	# TODO: like ignitables, get all events from nodes
-	# only keeping the valid ones in case some are empty
-	# later, also keep ignitable as node refs instead of node paths,
-	# so we don't have to get nodes from path again
-	# (and we don't have to erase invalid paths either... just create a list of valid
-	# nodes to start with)
-	
-	var invalid_ignitable_paths := []
-	
 	for ignitable_path in trigger_ignitable_paths:
-		if ignitable_path.is_empty():
-			invalid_ignitable_paths.append(ignitable_path)
-			print("WARNING: IgnitionEvent '%s' references trigger ignitable with empty path. It will be removed at runtime, but you should delete it in the Inspector" % get_path())
-			continue
+		var ignitable = get_node(ignitable_path) as Ignitable
+		NodeUtils.assert_node_got_by_path(ignitable, "IgnitionTrigger", self, "Ignitable", ignitable_path)
 		
-		var ignitable = get_node(ignitable_path)
-		if not ignitable:
-			invalid_ignitable_paths.append(ignitable_path)
-			print("WARNING: IgnitionEvent '%s' references trigger ignitable with path: '%s', but it is null. It will be removed at runtime." % [get_path(), ignitable_path])
-			continue
-		
-		ignitable = ignitable as Ignitable
-		if not ignitable:
-			invalid_ignitable_paths.append(ignitable_path)
-			print("WARNING: Door '%s' references trigger ignitable %s, but it is not an Ignitable. It will be removed at runtime." % [get_path(), ignitable_path])
-			continue
-
 		# deferred connection to avoid disabling collision during physics process (only matters for lit signal)
 		var error1 = ignitable.connect("lit", self, "_on_trigger_ignitable_lit", [], CONNECT_DEFERRED)
 		var error2 = ignitable.connect("unlit", self, "_on_trigger_ignitable_unlit", [], CONNECT_DEFERRED)
 		if error1 or error2:
-			print("WARNING: Ignitable connection failed with error1: %s and error2: %s. Disconnect will fail later." % [error1, error2])
+			print("WARNING: Ignitable connection failed with error1: %s and error2: %s. Ignoring this ignitable." % [error1, error2])
 			continue
-		
-		_trigger_ignitables.append(ignitable)
 		
 		# only count valid ignitables to avoid getting stuck in case
 		# we prepared an array too big (e.g. size = 4 but filled only 3)
-		_trigger_ignitable_count += 1
+		_trigger_ignitables.append(ignitable)
 		
-	# cleanup any invalid node path (just so _trigger doesn't have to check them again before disconnect)
-	for invalid_ignitable_path in invalid_ignitable_paths:
-		trigger_ignitable_paths.erase(invalid_ignitable_path)
+		_trigger_ignitable_count += 1
 
 	for event_path in event_paths:
 		var event := get_node(event_path) as Event
-		NodeUtils.assert_node_got_by_path(event, typeof(self), self, "Event", event_path)
+		NodeUtils.assert_node_got_by_path(event, "IgnitionTrigger", self, "Event", event_path)
 		_events.append(event)
 
 func _on_trigger_ignitable_lit():
@@ -92,6 +65,7 @@ func _on_trigger_ignitable_unlit():
 func _trigger_event():
 	for event in _events:
 		event.trigger()
+	
 	_disconnect_ignitables()
 
 func _disconnect_ignitables():
