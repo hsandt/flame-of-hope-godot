6bd77fc7681eb67bb807d38146e1f67ee454df44
huulong <n.huu.long@gmail.com>
2023-05-08 21:35:45 +0200

    [Door] Replaced proto red rectangle with small animated flames (reused) and adjust collider

--- Scripts/Props/Door.gd
+++ Scripts/Props/Door.gd
@@ -1,7 +1,13 @@
 class_name Door
-extends Sprite
+extends Node2D
 
 onready var collision_shape := $StaticBody2D/CollisionShape2D as CollisionShape2D
+onready var animated_sprite := $AnimatedSprite as AnimatedSprite
+onready var animated_sprite2 := $AnimatedSprite2 as AnimatedSprite
+
+func _ready():
+	animated_sprite.play()
+	animated_sprite2.play()
 
 func open():
 	visible = false
