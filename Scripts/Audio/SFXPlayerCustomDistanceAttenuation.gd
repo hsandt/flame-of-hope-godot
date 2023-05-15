a57fad37539b4b070ebf0a3c525931add10fa321
huulong <n.huu.long@gmail.com>
2023-05-15 15:50:26 +0200

    [SFX] Better sound attenuation: preserve uniform volume inside room/camera rectangle, but still drop as before outside (that makes a gap in volume)

--- Scripts/Audio/SFXPlayerCustomDistanceAttenuation.gd
+++ Scripts/Audio/SFXPlayerCustomDistanceAttenuation.gd
@@ -16,6 +16,9 @@ export(float) var max_distance = 300.0
 
 var base_volume_db: float
 var playercam: Camera2D
+var native_width: float
+var native_height: float
+
 
 func _ready():
 	# Store initial volume DB set in scene
@@ -25,13 +28,27 @@ func _ready():
 	# is merged, so for now, retrieve it via group
 	playercam = get_tree().get_nodes_in_group("camera")[0]
 
+	var viewport = get_viewport()
+	native_width = viewport.get_visible_rect().size.x
+	native_height = viewport.get_visible_rect().size.y
+
 
 func set_volume_from_current_distance_to_camera():
-	# global_transform not found on Godot 3.5.2, as Node doesn't have position
-	# instead take parent's position
-	var distance: float = get_parent().global_position.distance_to(playercam.get_camera_screen_center())
-	var linear_energy: float = max(0.0, 1.0 - (distance / max_distance))
-	volume_db = base_volume_db + linear2db(linear_energy)
+	var camera_center := playercam.get_camera_screen_center()
+	var parent_pos: Vector2 = get_parent().global_position
+	
+	if abs(parent_pos.x - camera_center.x) <= native_width / 2.0 and \
+			abs(parent_pos.y - camera_center.y) <= native_height / 2.0:
+		# Inside room rectangle (which is also camera rectangle), keep uniform volume
+		volume_db = base_volume_db
+	else:
+		# Else, decrease volume with distance
+		# Note that we still use distance from camera center, not camera edge,
+		# so there is indeed a gap between the camera edge and the outer area,
+		# where the volume suddenly drops
+		var distance: float = parent_pos.distance_to(camera_center)
+		var linear_energy: float = max(0.0, 1.0 - (distance / max_distance))
+		volume_db = base_volume_db + linear2db(linear_energy)
 
 
 # override
