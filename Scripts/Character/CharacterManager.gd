247f2fc7621083ac96569b69aa1b90991f8276c4
huulong <n.huu.long@gmail.com>
2023-05-15 19:33:35 +0200

    [Cheat] Debug only: press R to restart. Also move camera immediately, only enable smooth after 0.1s
    (applies in release but effect not visible since camera starts at the target position)
    
    Also add groups for player_character and character_manager, unused for convenient

--- Scripts/Character/CharacterManager.gd
+++ Scripts/Character/CharacterManager.gd
@@ -6,5 +6,27 @@ onready var character := $"../Character" as Node2D
 export(int) var start_room_index = 0
 
 func _ready():
+	# Note that this is run even in release, so we can place character and camera
+	# where we want immediately in case they are not preset correctly in the level
+	
 	# room 1 entrance is at 240, 268 and each room has a height of 16*17=272
-	character.global_position = Vector2(240, 268 - 272 * start_room_index)
+	_warp_character(Vector2(240, 268 - 272 * start_room_index))
+
+	# Wait a short time, just enough to load/reload the game if using restart
+	# and so character enters RoomEntrance and triggers camera motion
+	# with no smoothing so it is immediate the first time;
+	# but short enough to guarantee character won't enter next room in that duration)
+	yield(get_tree().create_timer(0.1), "timeout")
+
+	# Then enable smoothing so camera moves smoothly on further Room Entrances
+	var camera: Camera2D = get_tree().get_nodes_in_group("camera")[0]
+	camera.smoothing_enabled = true
+	
+
+func _unhandled_input(event):
+	if OS.has_feature("debug"):
+		if event.is_action_pressed("cheat_restart"):
+			var _err = get_tree().reload_current_scene()
+
+func _warp_character(position: Vector2):
+	character.global_position = position
