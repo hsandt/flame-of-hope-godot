c590efd8db50baf4374724d9df320159700b9d53
huulong <n.huu.long@gmail.com>
2023-06-05 15:11:18 +0200

    [Light] Fix tween errors on repeated ScaleFlicker scale to zero
    by checking if tween is still valid, and only reuse it to play if so

--- Scripts/Visual/ScaleFlicker.gd
+++ Scripts/Visual/ScaleFlicker.gd
@@ -10,6 +10,10 @@ var _tween_flicker: SceneTreeTween
 var _tween_scale_to_zero: SceneTreeTween
 
 
+# Since show_with_flicker and hide_with_scale_to_zero can be called in other scripts'
+# _ready, we cannot count on our _ready to initialize stuff (we could wait one frame
+# if we really wanted to) instead, we lazily init when we need to with the methods below
+	
 func _init_tween_flicker():
 	_tween_flicker = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).set_loops()
 	var _scale_tweener1 = _tween_flicker.tween_property(self, "scale", flicker_max_scale * Vector2.ONE, flicker_period / 2.0).from(1.0 * Vector2.ONE)
@@ -28,23 +32,27 @@ func show_with_flicker():
 	# actually show the node
 	visible = true
 
-	# since this can be called in other scripts'  _ready, we cannot count on
-	# on our _ready to initialize stuff (we could wait one frame if we really wanted to)
-	# instead, we lazily init when we need to
-	if _tween_flicker == null:
-		# this auto-plays, so no need to play manually
-		_init_tween_flicker()
-	else:
+	# In Godot 3, we use SceneTreeTweens which cannot be reused after kill/finish
+	# because Tween::clear sets valid to false, and it's not reversible
+	# (play() would error "Tween invalid. Either finished or created outside scene tree.")
+	# In this case _tween_flicker is looping indefinitely so we know it's gonna be valid
+	# and we can reuse it, but to be generic and safe, we still check it
+	if _tween_flicker != null and _tween_flicker.is_valid():
 		# play the existing one
 		_tween_flicker.play()
+	else:
+		# this auto-plays, so no need to play manually
+		_init_tween_flicker()
 
 func hide_with_scale_to_zero():
 	# cancel any concurrent tween
 	if _tween_flicker != null and _tween_flicker.is_running():
 		_tween_flicker.stop()
 	
-	# same principle as show_with_flicker
-	if _tween_scale_to_zero == null:
-		_init_tween_scale_to_zero()
-	else:
+	# same principle as show_with_flicker, except _tween_scale_to_zero is
+	# a short one-time tween so most of the time after the first tween,
+	# it has previously finished and is not valid so we must really check validity
+	if _tween_scale_to_zero != null and _tween_scale_to_zero.is_valid():
 		_tween_scale_to_zero.play()
+	else:
+		_init_tween_scale_to_zero()
