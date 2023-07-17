f35aa5b254a50c7287d15c41abf7fd8cb35c3ae8
huulong <n.huu.long@gmail.com>
2023-07-17 16:56:24 +0200

    [SFX] Fix SFXManager broken after Restart due to Dungeon being reloaded and reference being lost
    Retrieve reference on spawn_sfx

--- Scripts/Audio/SFXManager.gd
+++ Scripts/Audio/SFXManager.gd
@@ -2,9 +2,11 @@ extends Node
 
 export(PackedScene) var temp_sfx_player_prefab
 
-onready var dungeon = $"/root/Dungeon" as Node2D
-
 func spawn_sfx(audio_stream: AudioStream):
+	# Retrieve dungeon each time (or at least once after each Restart)
+	# instead of just in onready since Restart causes full level reload,
+	# which would free dungeon and cause invalid reference on further use
+	var dungeon = $"/root/Dungeon" as Node2D
 	assert(audio_stream != null, "SFXManager.spawn_sfx: expected audio_stream")
 	var temp_sfx_player: TempSFXPlayer = temp_sfx_player_prefab.instance()
 	dungeon.add_child(temp_sfx_player)
